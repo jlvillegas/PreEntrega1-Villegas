2e89503fd5a196b1da1f5a320d1c5c454ba76c1b
jlvillegas <lvillegasjorge@gmail.com>
2023-07-31 09:22:00 -0300

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+function Footer() {
+  return (
+    <footer id="footer" className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400"  >
+      <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            </a>
+      <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+      <p>&copy; copyright 2023 - Jorge Luis Villeagas</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+
+function Header() {
+  return (
+    <header id="header" className="header flex justify-between py-4 px-2 bg-slate-800 text-white" >
+      <h1>Mi pagina web</h1>
+      <nav>
+        <a href="#">LINK</a>
+        <a href="#">LINK</a>
+        <a href="#">LINK</a>
+        <a href="#">LINK</a>
+      </nav>
+
+    </header>
+    
+  )
+}
+
+export default Header
